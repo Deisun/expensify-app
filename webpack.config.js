ae493ec423e38272b09320f5134f4f81fc7bdafc
Rob Douma <rdouma81@gmail.com>
2020-07-02 15:34:58 -0400

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react', {
+                        'plugins': ['@babel/plugin-proposal-class-properties']
+                    }]
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader']
+            }]
+    }
+};
