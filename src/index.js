fc728b557a0c5883ed8ea852f5d67dd18c88622f
Rob Douma <rdouma81@gmail.com>
2020-07-10 16:59:18 -0400

    HOC Higher Order Code

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ import {addExpense} from "./actions/expenses";
 import {setTextFilter} from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
-const store = configureStore();
-
-store.dispatch(addExpense({description: 'Water bill'}));
-store.dispatch(addExpense({description: 'Gas bill'}));
-store.dispatch(setTextFilter('water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-console.log(store.getState());
+// const store = configureStore();
+//
+// store.dispatch(addExpense({description: 'Water bill'}));
+// store.dispatch(addExpense({description: 'Gas bill'}));
+// store.dispatch(setTextFilter('water'));
+//
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
+//
+// console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
---
+++ src/playground/hoc.js
@@ -0,0 +1,5 @@
+// Higher Order Component (HOC) A component that renders another component.
+
+import React from "react";
+import ReactDom from 'react-dom';
+
