6c7d61d85fdbbcf84077d86439dd55d5b237e144
Deisun <rdouma81@gmail.com>
2020-07-14 23:31:36 -0400

    Controlled inputs for Filters.

--- src/index.js
+++ src/index.js
@@ -3,22 +3,29 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './styles/styles.scss'
 import AppRouter from "./routers/AppRouter";
+import { Provider } from 'react-redux';
 import configureStore from "./store/configureStore";
 import {addExpense} from "./actions/expenses";
 import {setTextFilter} from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
-// const store = configureStore();
-//
-// store.dispatch(addExpense({description: 'Water bill'}));
-// store.dispatch(addExpense({description: 'Gas bill'}));
-// store.dispatch(setTextFilter('water'));
-//
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-//
-// console.log(store.getState());
+const store = configureStore();
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+console.log(store.getState());
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('root'));
 
