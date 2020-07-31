500433f7f58f95b2b1ae82efac5fcc11b365943f
Rob Douma <rdouma81@gmail.com>
2020-07-31 12:19:39 -0400

    Testing with Jest

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {connect} from "react-redux";
-import {removeExpense} from "../actions/expenses";
 import {Link} from "react-router-dom";
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
