66c3ffc726790224ca9b9972644104c61f77e0a3
Deisun <rdouma81@gmail.com>
2020-08-11 17:32:05 -0400

    Implement Webpack production and dev modes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react', {
-                        'plugins': ['@babel/plugin-proposal-class-properties']
-                    }]
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
+
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', {
+                            'plugins': ['@babel/plugin-proposal-class-properties']
+                        }]
+                    },
+                    test: /\.js$/,
+                    exclude: /node_modules/
                 },
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }]
+                {
+                    test: /\.s?css$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map'
     }
 };
+
