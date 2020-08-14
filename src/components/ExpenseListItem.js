35de566d393af5884a5830244ddebbca8e2843fb
Rob Douma <rdouma81@gmail.com>
2020-08-14 10:30:49 -0400

    Implement currency and date formatting on expense list.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { format } from 'date-fns'
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')} - {format(createdAt, 'MMMM do, yyyy')}
+        </p>
     </div>
 );
 
