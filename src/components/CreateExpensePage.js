9c3a59aeafa687a997150f08b6386226d2d2bd9f
Rob Douma <rdouma81@gmail.com>
2020-08-28 10:03:44 -0400

    Implement more tests for ExpenseListFilters

--- src/components/CreateExpensePage.js
+++ src/components/CreateExpensePage.js
@@ -3,18 +3,41 @@ import ExpenseForm from "./ExpenseForm";
 import {connect} from 'react-redux';
 import {startAddExpense} from "../actions/expenses";
 
-const CreateExpensePage = (props) => (
-    <div>
-        <h3>Create Expense</h3>
-        <ExpenseForm
-            onSubmit={(expense) => {
-                props.dispatch(startAddExpense(expense));
-                // uses browser routing to go back to dashboard
-                props.history.push('/');
-                console.log("created: ", expense)
-            }}
-        />
-    </div>
-)
+export class CreateExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.addExpense(expense);
+        this.props.history.push('/');
+    };
+    render() {
+        return (
+            <div>
+                <h3>Create Expense</h3>
+                <ExpenseForm
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+    }
+}
 
-export default connect()(CreateExpensePage);
+// const CreateExpensePage = (props) => (
+//     <div>
+//         <h3>Create Expense</h3>
+//         <ExpenseForm
+//             onSubmit={(expense) => {
+//                 //props.dispatch(startAddExpense(expense));
+//                 props.onSubmit(expense);
+//
+//                 // uses browser routing to go back to dashboard
+//                 props.history.push('/');
+//                 console.log("created: ", expense)
+//             }}
+//         />
+//     </div>
+// )
+
+const mapDispatchToProps = (dispatch) => ({
+    addExpense: (expense) => dispatch(startAddExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(CreateExpensePage);
