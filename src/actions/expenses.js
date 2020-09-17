1f653f53d81894cf209f09b6778a182a011bfb73
Rob Douma <rdouma81@gmail.com>
2020-09-17 09:48:07 -0400

    Firebase and Redux working together for creating expenses.

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -9,12 +9,14 @@ export const addExpense = (expense) => ({
 
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
+
         const {
             description = '',
             note = '',
             amount = 0,
             createdAt = 0
         } = expenseData;
+
         const expense = { description, note, amount, createdAt };
 
         database.ref('expenses').push(expense).then((ref) => {
