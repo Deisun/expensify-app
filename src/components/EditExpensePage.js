f15a9395a30043a18205180c74e6447c5c2b1372
Rob Douma <rdouma81@gmail.com>
2020-09-24 09:28:24 -0400

    Implement Remove Expense with Firebase.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {connect} from 'react-redux';
 import ExpenseForm from "./ExpenseForm";
-import {editExpense, removeExpense} from "../actions/expenses";
+import {editExpense, startRemoveExpense } from "../actions/expenses";
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
@@ -9,7 +9,7 @@ export class EditExpensePage extends React.Component {
         this.props.history.push('/');
     };
     onRemove = () => {
-        this.props.removeExpense({ id: this.props.expense.id });
+        this.props.startRemoveExpense({ id: this.props.expense.id });
         this.props.history.push('/');
     };
     render() {
@@ -25,31 +25,13 @@ export class EditExpensePage extends React.Component {
     }
 };
 
-// const EditExpensePage = (props) => {
-//     return (
-//         <div>
-//             <ExpenseForm
-//                 expense={props.expense}
-//                 onSubmit={(expense) => {
-//                     props.dispatch(editExpense(props.expense.id, expense));
-//                     props.history.push('/');
-//                 }}
-//             />
-//             <button onClick={() => {
-//                 props.dispatch(removeExpense({ id: props.expense.id }));
-//                 props.history.push('/');
-//             }}>Remove</button>
-//         </div>
-//     );
-// };
-
 const mapStateToProps = (state, props) => ({
     expense: state.expenses.find((expense) => expense.id === props.match.params.id)
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
     editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-    removeExpense: (data) => dispatch(removeExpense(data))
+    startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
