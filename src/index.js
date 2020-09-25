d46fe06467a2a132cb100532ef97f7ef8352a255
Rob Douma <rdouma81@gmail.com>
2020-09-25 08:29:38 -0400

    Login and Logout re-directing.

--- src/index.js
+++ src/index.js
@@ -32,14 +32,14 @@ firebase.auth().onAuthStateChanged((user) => {
    if (user) {
        console.log("log in")
        store.dispatch(startSetExpenses()).then(() => {
-           renderApp()
            if (history.location.pathname === '/') {
                history.push('/dashboard')
            }
+           renderApp()
        });
    } else {
        console.log("log out")
-       renderApp()
        history.push('/')
+       renderApp()
    }
 });
