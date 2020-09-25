98fcd1d426918b32b18f4137d3d0ef39bd285468
Rob Douma <rdouma81@gmail.com>
2020-09-25 08:38:24 -0400

    Login and Logout re-directing.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Router, Route, Switch} from "react-router-dom";
 import CreateExpensePage from "../components/CreateExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
