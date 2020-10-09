3942fe771f0a6d004a971ac01b8a47051944a842
Rob Douma <rdouma81@gmail.com>
2020-10-09 11:55:11 -0400

    Finished project.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {login, logout} from "./actions/auth";
 import 'normalize.css';
 import './styles/styles.scss'
 import "react-datepicker/dist/react-datepicker.css";
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
 const jsx = (
@@ -26,7 +27,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+ReactDOM.render(<LoadingPage />, document.getElementById('root'));
 
 
 firebase.auth().onAuthStateChanged((user) => {
